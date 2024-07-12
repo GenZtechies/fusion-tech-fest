ed4a3121ec92746e92da4f994374ebac54f90f8e
John Olatubosun <toluolatubosun@gmail.com>
2024-07-12 21:19:12 +0100

    feat: mobile nav

--- src/components/custom/navbar.tsx
+++ src/components/custom/navbar.tsx
@@ -1,8 +1,13 @@
+"use client";
+
+import React from "react";
 import Link from "next/link";
-import ArrowRightCircleFilledIcon from "../svg-icons/arrow-right-circle-filled-icon";
-import MenuHamburgerIcon from "../svg-icons/menu-hamburger-icon";
-import { Button } from "../ui/button";
+
 import Logo from "./logo";
+import { Button } from "../ui/button";
+import CloseIcon from "../svg-icons/close-icon";
+import MenuHamburgerIcon from "../svg-icons/menu-hamburger-icon";
+import ArrowRightCircleFilledIcon from "../svg-icons/arrow-right-circle-filled-icon";
 
 const NAV_LINKS = [
     { name: "About", href: "/about" },
@@ -14,28 +19,48 @@ const NAV_LINKS = [
 ];
 
 function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
     return (
-        <div className="contained absolute top-9 left-1/2 -translate-x-1/2">
-            <nav className="flex items-center justify-between border border-[#36156619] rounded-full bg-[#ffffff19] py-2.5 px-5 ">
-                <Logo />
+        <>
+            <div className={`bg-hero-stars bg-[#FFF3FD] w-full h-screen fixed z-50 flex flex-col justify-between px-12 py-14 lg:hidden ${!isMenuOpen && "hidden"}`}>
+                <div className="flex items-end justify-end">
+                    <Button
+                        onClick={() => setIsMenuOpen(false)}
+                    >
+                        Close <CloseIcon className="ml-2" />
+                    </Button>
+                </div>
 
-                <ul className="hidden lg:flex items-center space-x-4">
-                    {NAV_LINKS.map((link) => (
-                        <li key={link.name} className="font-helvetica-compressed text-lg text-[#361566] tracking-wider">
-                            <Link href={link.href}>{link.name}</Link>
-                        </li>
-                    ))}
-                </ul>
+                <div>
+                    <h1>Hi</h1>
+                </div>
+            </div>
+            <div className="contained absolute top-9 left-1/2 -translate-x-1/2">
+                <nav className="flex items-center justify-between border border-[#36156619] rounded-full bg-[#ffffff19] py-2.5 px-5 ">
+                    <Logo />
 
-                <Button className="hidden lg:flex">
-                    Register <ArrowRightCircleFilledIcon className="ml-2" />
-                </Button>
+                    <ul className="hidden lg:flex items-center space-x-4">
+                        {NAV_LINKS.map((link) => (
+                            <li key={link.name} className="font-helvetica-compressed text-lg text-[#361566] tracking-wider">
+                                <Link href={link.href}>{link.name}</Link>
+                            </li>
+                        ))}
+                    </ul>
 
-                <Button className="lg:hidden">
-                    Menu <MenuHamburgerIcon className="ml-2" />
-                </Button>
-            </nav>
-        </div>
+                    <Button className="hidden lg:flex">
+                        Register <ArrowRightCircleFilledIcon className="ml-2" />
+                    </Button>
+
+                    <Button
+                        onClick={() => setIsMenuOpen((prev) => !prev)}
+                        className="lg:hidden"
+                    >
+                        Menu <MenuHamburgerIcon className="ml-2" />
+                    </Button>
+                </nav>
+            </div>
+        </>
     );
 }
 
